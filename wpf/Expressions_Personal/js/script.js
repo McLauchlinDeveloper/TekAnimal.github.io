5cb4eca2252457bf9d5feaac30e88e162bf20b5d
Corey M <CoreyRyan@fullsail.edu>
2014-02-10 10:18:46 -0500

    Expressions_Personal

--- wpf/Expressions_Personal/js/script.js
+++ wpf/Expressions_Personal/js/script.js
@@ -3,11 +3,16 @@
  Expressions_Personal
  */
 
+
+// declaring, defining, and storing variable for budget for year
 var budget = prompt("What is your entire budget for the year?");
 
+// declaring, defining, and storing variable for food out of budget
 var food = prompt("If you make " + budget + " a year, how much will be spent on food?");
 
+// delalring, defining, and storing variable for rent out of budget
 var rent = prompt("How much will be spent on rent out of the " + budget + "?");
 
+// declaring, defining, and storing variable for other expenses out of budget
 var other = prompt("How much of the " + budget + " will be spent on other expenses?");
 
