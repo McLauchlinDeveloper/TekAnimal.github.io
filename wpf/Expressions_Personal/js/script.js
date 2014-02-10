37e5043f5beb64df10ca88750993867025c76a9e
Corey M <CoreyRyan@fullsail.edu>
2014-02-10 11:20:42 -0500

    Expressions_Personal

--- wpf/Expressions_Personal/js/script.js
+++ wpf/Expressions_Personal/js/script.js
@@ -1,6 +1,7 @@
 /*
  Corey McLauchlin
  Expressions_Personal
+ script calculates how much money is spent out of a yearly budget in percent
  */
 
 
@@ -10,9 +11,13 @@ var budget = prompt("What is your entire budget for the year?");
 // declaring, defining, and storing variable for food out of budget
 var food = prompt("If you make " + budget + " a year, how much will be spent on food?");
 
-// delalring, defining, and storing variable for rent out of budget
+// declaring, defining, and storing variable for rent out of budget
 var rent = prompt("How much will be spent on rent out of the " + budget + "?");
 
 // declaring, defining, and storing variable for other expenses out of budget
 var other = prompt("How much of the " + budget + " will be spent on other expenses?");
 
+//calculating food percentage
+var foodPercent = food / budget * 100;
+// parse food percent and only keep 2 decimal places
+var foodPercentage = parseFloat(foodPercent).toFixed(2);
