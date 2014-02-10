c6a98f8d041778f4c3eb9076e9c04b41910b60fb
Corey M <CoreyRyan@fullsail.edu>
2014-02-10 10:13:20 -0500

    Expressions_Personal
    v4

--- wpf/Expressions_Personal/js/script.js
+++ wpf/Expressions_Personal/js/script.js
@@ -7,3 +7,4 @@ var budget = prompt("What is your entire budget for the year?");
 
 var food = prompt("If you make " + budget + " a year, how much will be spent on food?");
 
+var rent = prompt("How much will be spent on rent out of the " + budget + "?");
