32caf3723e76f6fdd605a098efac3c66cf593484
Corey M <CoreyRyan@fullsail.edu>
2014-02-10 10:04:45 -0500

    Expressions_Personal
    v3

--- wpf/Expressions_Personal/js/script.js
+++ wpf/Expressions_Personal/js/script.js
@@ -3,3 +3,7 @@
  Expressions_Personal
  */
 
+var budget = prompt("What is your entire budget for the year?");
+
+var food = prompt("If you make " + budget + " a year, how much will be spent on food?");
+
