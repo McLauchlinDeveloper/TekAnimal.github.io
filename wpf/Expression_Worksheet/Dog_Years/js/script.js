bd025e32eab1be77bcd839c6248df557373d7782
Corey M <CoreyRyan@fullsail.edu>
2014-02-10 13:31:01 -0500

    Expression_Worksheet

---
+++ wpf/Expression_Worksheet/Dog_Years/js/script.js
@@ -0,0 +1,16 @@
+/*
+
+Corey McLauchlin
+Feb. 10, 2014
+Expression_Worksheet Dog Years
+ */
+
+//sparky's age in human years
+
+var dogAge = 5;
+// calculates sparky's age in dog years
+var dogYears = dogAge * 7;
+
+// Result of calculation of sparky's age
+
+console.log("Sparky's age in human years is " + dogAge + " old, which is " + dogYears + " in dog years.");
