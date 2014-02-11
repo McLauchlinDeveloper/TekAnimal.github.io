afee5c35cf1c76a5ffb24991d0ae1c459e96ab9d
Corey M <CoreyRyan@fullsail.edu>
2014-02-11 00:12:59 -0500

    Expressions_Wacky

--- wpf/Expressions_Wacky/js/script.js
+++ wpf/Expressions_Wacky/js/script.js
@@ -1,3 +1,24 @@
 /**
- * Created by bangalorn on 2/7/14.
+ Corey McLauchlin
+ Feb. 10, 2014
+ Expressions_Wacky
+ calculates density
  */
+// define variable and prompt user for length
+var length = prompt("What is the length of your object?");
+// define variable and prompt user for width
+var width = prompt("What is the width of your object?");
+// define variable and prompt user for height
+var height = prompt("What is the height of your object?");
+// define variable and prompt user for mass
+var mass = prompt("What is the mass(weight) of the object?");
+// define array and store variables
+var densCalc = [length, width, height, mass];
+// calculate volume with stored, prompted variables
+var volume = densCalc[0] * densCalc[1] * densCalc[2];
+// calculate density using array variables
+var dense = densCalc[3] / volume;
+// clean up density output by rounding decimal places
+var density = dense.toFixed(4);
+// output of density to user
+console.log("The density of your object is " + density + ".");
