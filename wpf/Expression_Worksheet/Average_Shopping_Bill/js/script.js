19fd2e64ee57d3157da96ea8695d83e4d6411330
Corey M <CoreyRyan@fullsail.edu>
2014-02-13 13:00:33 -0500

    Expressions_Worksheet

---
+++ wpf/Expression_Worksheet/Average_Shopping_Bill/js/script.js
@@ -0,0 +1,10 @@
+// Expressions_Worksheet
+
+
+var totals[4] = [266, 350, 400, 198, 213];
+
+var totalsAdd = totals[0]+totals[1]+totals[2]+totals[3]+totals[4];
+
+var totaled = totalsAdd / 5;
+
+console.log("You have spent a total of $" + totalsAdd + " on groceries over 5 weeks. That is an average of $"+totaled+" per week.");
