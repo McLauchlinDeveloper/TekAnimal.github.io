39b693ae3ee20d6dc71a5de985121ba0bda9d47e
Corey M <CoreyRyan@fullsail.edu>
2014-02-16 18:18:53 -0500

    Conditionals

---
+++ wpf/Conditionals/Ternary_Operators/js/script.js
@@ -0,0 +1,15 @@
+/**
+ * Created by bangalorn on 2/16/14.
+ */
+//Ternary operators
+
+var gpa = 48;
+
+//if the gpa is over the min 2.0 score, the student can graduate
+/*
+if(gpa > 2.0){
+    console.log("You can graduate!");
+}else{
+    console.log("GPA too low!");
+}*/
+
