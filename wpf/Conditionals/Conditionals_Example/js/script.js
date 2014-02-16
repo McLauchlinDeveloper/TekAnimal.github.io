6f4634607b20b36e8835360a940d38ace9f2eefb
Corey M <CoreyRyan@fullsail.edu>
2014-02-16 14:49:21 -0500

    Coditionals

---
+++ wpf/Conditionals/Conditionals_Example/js/script.js
@@ -0,0 +1,12 @@
+// Conditional Logic
+
+var oldEnough = true;
+
+// if the child is old enough, print to the console "youcanride!"
+if(oldEnough)
+{
+   // code performed if condition is true
+
+    console.log("you can ride the coaster!");
+}
+console.log("what comes after");
