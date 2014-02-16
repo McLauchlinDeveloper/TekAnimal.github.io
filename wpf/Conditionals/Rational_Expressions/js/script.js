c8238bf085c2f515bd6ebc500a7bcd57195d651e
Corey M <CoreyRyan@fullsail.edu>
2014-02-16 14:58:55 -0500

    Conditionals

--- wpf/Conditionals/Rational_Expressions/js/script.js
+++ wpf/Conditionals/Rational_Expressions/js/script.js
@@ -2,7 +2,7 @@
  * Created by bangalorn on 2/16/14.
  */
 
-var kidHeight = 40;
+var kidHeight = 50;
 var minHeight = 48
 //if the child is old enough, print to the console "you can ride";
 //if the kid is over 48 inches in height
