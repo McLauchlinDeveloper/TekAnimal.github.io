dc6c0008eca02feabfdd70e8b22682e10a9d1f0e
Corey M <CoreyRyan@fullsail.edu>
2014-02-16 14:54:51 -0500

    Coditionals

---
+++ wpf/Conditionals/Rational_Expressions/js/script.js
@@ -0,0 +1,12 @@
+/**
+ * Created by bangalorn on 2/16/14.
+ */
+
+var kidHeight = 30;
+
+//if the child is old enough, print to the console "you can ride";
+
+if(oldEnough){
+
+    console.log("You can ride the coaster!");
+}
