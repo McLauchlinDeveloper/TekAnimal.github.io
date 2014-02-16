e079bacf585e11dc45d110c393045b161adc9b7d
Corey M <CoreyRyan@fullsail.edu>
2014-02-16 15:33:32 -0500

    Conditionals_If_Else

---
+++ wpf/Conditionals/If_Else/js/script.js
@@ -0,0 +1,17 @@
+//Conditional Logic - With an Expression
+
+var kidHeight = 47;
+var minHeight = 48;
+
+// if the child is old enough, print to the console "you can ride!"
+// if the kid is over 48 inches in height
+
+if(kidHeight > minHeight){
+    // code performed if condition is true
+    console.log("You can ride the coaster!");
+
+}else{
+
+   // code performed if condition is false
+    console.log("You need to grow some more!");
+}
