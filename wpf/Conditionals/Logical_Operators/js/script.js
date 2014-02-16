c06336625b5a725b0ed77f174afd6e275831e134
Corey M <CoreyRyan@fullsail.edu>
2014-02-16 16:00:59 -0500

    Logical_Operators

--- wpf/Conditionals/Logical_Operators/js/script.js
+++ wpf/Conditionals/Logical_Operators/js/script.js
@@ -4,5 +4,18 @@
 //conditional logic - logical operators
 
 var budget = 300;
-var iPhonePrice = 19;
+var iPhonePrice = 199.99;
+var paycheck= 200;
+//if the price of the phone is less our budget AND if our paycheck is over 300
+if(iPhonePrice < budget)}{
 
+    console.log("We can buy the phone!");
+    }
+else{
+    console.log("No phone for you!");
+}
+if (paycheck > 300){
+    console.log("We can buy the phone!");
+}else {
+    console.log("No phone for you!");
+}
