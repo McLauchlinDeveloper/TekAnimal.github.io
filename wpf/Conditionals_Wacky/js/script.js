cd94b6e9d664544ec73ce07af10031d78e83c500
Corey M <CoreyRyan@fullsail.edu>
2014-02-19 01:00:15 -0500

    Conditionals_Wacky

---
+++ wpf/Conditionals_Wacky/js/script.js
@@ -0,0 +1,10 @@
+/**
+ * Created by bangalorn on 2/19/14.
+ */
+// calculates whether a specific jump is hard and high enough to clear the stairs
+//greeting
+console.log("Today we are going to calculate how easily you can get down a set of 10 stairs on a skateboard.")
+// prompt and assign info to variable
+var weight = prompt("How much do you weigh (enter number of lbs)");
+// makes sure the proper value was assigned
+(weight == "" || weight < 60) ? (console.log("You did not answer properly or will not make it down the stairs.")):(console.log("You weigh "+weight+"lbs. "));
