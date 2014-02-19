b5bcb3547d1b70fc9bf1987f5b1b28a18a290723
Corey M <CoreyRyan@fullsail.edu>
2014-02-19 01:36:13 -0500

    Conditionals_Wacky

--- wpf/Conditionals_Wacky/js/script.js
+++ wpf/Conditionals_Wacky/js/script.js
@@ -6,5 +6,9 @@
 console.log("Today we are going to calculate how easily you can get down a set of 10 stairs on a skateboard.")
 // prompt and assign info to variable
 var weight = prompt("How much do you weigh (enter number of lbs)");
-// makes sure the proper value was assigned
+// determines if captured data is suitable (would add loop and elseif if it wasn't ternary)
 (weight == "" || weight < 60) ? (console.log("You will not make it down the stairs or did not answer properly.")):(console.log("Your weight will make it down the stairs."));
+// convert weight in lbs to weight in kg
+var weightKg = weight * 2.2;
+
+var heightJmp = prompt("How high did you jump? ")
