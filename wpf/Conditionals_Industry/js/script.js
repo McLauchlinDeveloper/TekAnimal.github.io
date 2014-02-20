1d2ffa2e7a60aa5fefd86ab1efb10f7908c7215a
Corey M <CoreyRyan@fullsail.edu>
2014-02-20 14:05:22 -0500

    Conditionals_Industry

--- wpf/Conditionals_Industry/js/script.js
+++ wpf/Conditionals_Industry/js/script.js
@@ -5,17 +5,17 @@ Conditionals_Industry
 calculates whether a client will be able to a have a site made
  */
 // asks user how much money they have for site and stores it
-var money = prompt("How much money do you have to spend on your website?");
+var money = prompt("How much money do you have to spend on your website? (Preferably over $199)");
 
-if(money == "" || money == 0){console.log("We cannot make a site for you at this time.");}
+if(money == "" || money <= 199){console.log("We cannot make a site for you at this time.");}
 // stores what kind of site the user wants created
 var site = prompt("What kind of site do you need? (Select 1-3)\n 1.  Business Site\n2.  Personal Blog\n3.  Social Media Site");
 // if use picks business site, tell them so
-if(site == 1) {console.log("We would be happy to make a business site for you.");
-} else if(site == 2){
+if(site == 1 && money >= 200) {console.log("We would be happy to make a business site for you.");
+} else if(site == 2 && money >= 200){
 // if user picks personal blog, tell them so
     console.log("We would be happy to make a personal blog for you.");
-} else if(site == 3){
+} else if(site == 3 && money >= 200){
 // if user picks social media, make sure you know they know
     console.log("We would love to make a social media site for you.");
 }
