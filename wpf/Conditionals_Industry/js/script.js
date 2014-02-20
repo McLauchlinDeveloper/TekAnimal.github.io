a229630c347131ea05a0deac75edece58bd32569
Corey M <CoreyRyan@fullsail.edu>
2014-02-20 11:03:59 -0500

    Conditionals_Industry

--- wpf/Conditionals_Industry/js/script.js
+++ wpf/Conditionals_Industry/js/script.js
@@ -2,6 +2,16 @@
 Corey McLauchlin
 Feb. 20, 2014
 Conditionals_Industry
+calculates whether a client will be able to a have a site made
  */
+// asks user how much money they have for site and stores it
+var money = prompt("How much money do you have to spend on your website?");
 
-var site = ""
+var siteType = prompt("What kind of site do you need? (Select 1-3)\n 1.  Business Site\n2.  Personal Blog\n3.  Social Media Site");
+
+if(siteType = 1 && money > 1000){
+    console.log("We would be happy to make a business site for you!")
+} elseif(siteType = 2 && money > 250) {
+
+    
+}
