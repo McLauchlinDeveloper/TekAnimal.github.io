6f6f7076c51a566610805852aedd167a814a971e
Corey M <CoreyRyan@fullsail.edu>
2014-02-20 14:25:28 -0500

    Conditionals_Personal

--- wpf/Conditionals_Personal/js/script.js
+++ wpf/Conditionals_Personal/js/script.js
@@ -4,6 +4,8 @@ Feb. 20, 2014
 Conditionals_Personal
  */
 
-var memory = prompt("How much memory does your phone have?  (Enter in MB)");
+var mem = prompt("How much memory does your phone have?  (Enter in MB)");
+
+var memUse = prompt("How many MBs of memory are in use?");
 
 
