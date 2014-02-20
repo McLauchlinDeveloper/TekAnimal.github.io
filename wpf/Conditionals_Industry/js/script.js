29bdee986266e16ab86698409fa5517077200267
Corey M <CoreyRyan@fullsail.edu>
2014-02-20 13:47:08 -0500

    Conditionals_Industry

--- wpf/Conditionals_Industry/js/script.js
+++ wpf/Conditionals_Industry/js/script.js
@@ -7,15 +7,17 @@ calculates whether a client will be able to a have a site made
 // asks user how much money they have for site and stores it
 var money = prompt("How much money do you have to spend on your website?");
 
-if(money = ""){console.log("You do not have the money to have a site made for you");}
-
-var siteType = prompt("What kind of site do you need? (Select 1-3)\n 1.  Business Site\n2.  Personal Blog\n3.  Social Media Site");
-
-
-if(siteType = 1 && money >= 1000){
-    console.log("We would be happy to make a business site for you!");
-} else if(siteType = 2 && money >= 250) {
-    console.log("We would love to make your personal blog for you!");
-} else if(siteType = 3 && money >= 500) {
-    console.log("We will take pleasure in making your social media site!");
-} else {console.log("We unfortunately cannot help you make a site.");}
+if(money == "" || money == 0){console.log("We cannot make a site for you at this time.");}
+// stores what kind of site the user wants created
+var site = prompt("What kind of site do you need? (Select 1-3)\n 1.  Business Site\n2.  Personal Blog\n3.  Social Media Site");
+// if use picks business site, tell them so
+if(site == 1) {console.log("We would be happy to make a business site for you.");
+} else if(site == 2){
+// if user picks personal blog, tell them so
+    console.log("We would be happy to make a personal blog for you.");
+} else if(site == 3){
+// if user picks social media, make sure you know they know
+    console.log("We would love to make a social media site for you.");
+}
+// if user does not pick from list, site can't be made.
+else {console.log("We are sorry, but we cannot make you a website at this time.")}
