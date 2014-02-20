b018e5b90a40a72138242de98b27de9a7efc74eb
Corey M <CoreyRyan@fullsail.edu>
2014-02-20 06:42:57 -0500

    Conditionals_Wacky

--- wpf/Conditionals_Wacky/js/script.js
+++ wpf/Conditionals_Wacky/js/script.js
@@ -3,7 +3,7 @@ Corey McLauchlin
 Feb. 19, 2014
 
  */
-// calculates whether a specific jump is hard and high enough to clear the stairs
+// calculates whether a certain speed and weight will be enough to clear the stairs
 //greeting
 console.log("Today we are going to calculate how easily you can get down a set of 10 stairs on a skateboard.")
 // prompt and assign info to variable
@@ -16,8 +16,14 @@ var weightKg = weight * 2.2;
 var distance = prompt("What is the distance (in ft) between the starting point and point at which you jump down the stairs (runup)?");
 //converting feet to meters
 var distaMet = distance * .3048;
-
+// finding acceleration
 var accel = weightKg * 9.8;
-console.log("You will have a force of gravity on you equal to "+accel+" N (Newtons)");
+// outputing acceleration = force in gravity
+console.log("You will have a force of gravity (your acceleration) on you equal to "+accel+"N (Newtons).");
+// conditional estimating that amount of acceleration and momentum should exceed a base amount to get you down the stairs
+if(distaMet+accel >= 1000){
 
-console
+console.log("You will make it down the stairs SAFELY.");
+}else{
+    console.log("If you try, you may get hurt.");
+}
