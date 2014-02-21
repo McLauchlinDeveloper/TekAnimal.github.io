9bccc095c9d25c266aca76a39e0abf1af800d276
Corey M <CoreyRyan@fullsail.edu>
2014-02-20 21:24:33 -0500

    Conditionals_Personal

--- wpf/Conditionals_Personal/js/script.js
+++ wpf/Conditionals_Personal/js/script.js
@@ -8,4 +8,10 @@ var mem = prompt("How much memory does your phone have?  (Enter in MB)");
 
 var memUse = prompt("How many MBs of memory are in use?");
 
+var memPercent = (memUse / mem)*100;
+
+if(memPercent > 60)
+{
+    console.log("Memory has been cleared.");
+} else {console.log("Your memory does not be cleared yet.");}
 
