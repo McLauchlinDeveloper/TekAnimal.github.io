706ddf6b26ad831a74013d2eeaecebabb5c03bc2
Corey M <CoreyRyan@fullsail.edu>
2014-02-20 21:39:25 -0500

    Conditionals_Personal

--- wpf/Conditionals_Personal/js/script.js
+++ wpf/Conditionals_Personal/js/script.js
@@ -3,15 +3,18 @@ Corey McLauchlin
 Feb. 20, 2014
 Conditionals_Personal
  */
-
+//establishes how much memory the phone has
 var mem = prompt("How much memory does your phone have?  (Enter in MB)");
-
+// establishes how much of the memory is in use
 var memUse = prompt("How many MBs of memory are in use?");
-
+// converts the memory in use to a percentage
 var memPercent = (memUse / mem)*100;
-
+// if statement saying that if the memory is in use over a certain limit, in this case 60%
 if(memPercent > 60)
 {
+    // the memory has been cleared, telling user
     console.log("Memory has been cleared.");
-} else {console.log("Your memory does not be cleared yet.");}
+}
+//if the memory does not exceed use limit, it does not need to be cleared.
+else {console.log("Your memory does not be cleared yet.");}
 
