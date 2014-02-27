a95bdc1f7b730004109123004a2f6a1a1791319b
Corey M <CoreyRyan@fullsail.edu>
2014-02-27 01:36:35 -0500

    Anonymous_Function

--- wpf/Functions/Anonymous_Function/js/script.js
+++ wpf/Functions/Anonymous_Function/js/script.js
@@ -2,3 +2,14 @@
  * Created by bangalorn on 2/27/14.
  */
 
+
+
+var calcArea = function(width, height){ // defining
+
+    var area =width*height;
+    return area;
+}
+
+var a = calcArea(20, 30); //invoking
+
+console.log(a);
