487854dd9520bd86176e684b9f1c7092da142d29
Corey M <CoreyRyan@fullsail.edu>
2014-02-27 01:13:58 -0500

    Parameters_Arguments

--- wpf/Functions/Parameters_Arguments/js/script.js
+++ wpf/Functions/Parameters_Arguments/js/script.js
@@ -2,10 +2,10 @@
  * Created by bangalorn on 2/27/14.
  */
 
-calcArea(30, 20);
+function dogYears(){
 
-function calcArea(w, h){ // w=30, h = 20
-var area = w * h;
-    console.log(area);
-
+    var age = 4;
+    var dogYears = age * 7;
+    console.log("Sparky is" + dogYears + " years old.");
 }
+dogYears();
