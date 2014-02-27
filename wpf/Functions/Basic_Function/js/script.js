9221062720e9145f20c8640973088d4aa8afd613
Corey M <CoreyRyan@fullsail.edu>
2014-02-27 00:38:30 -0500

    Basic_Function

---
+++ wpf/Functions/Basic_Function/js/script.js
@@ -0,0 +1,13 @@
+// Basic Function
+
+function outptMsg(){
+    console.log("Hello");
+}
+
+function calcArea(){
+    var width = 20;
+    var height = 30;
+    var area = width * height;
+    console.log(area);
+}
+
