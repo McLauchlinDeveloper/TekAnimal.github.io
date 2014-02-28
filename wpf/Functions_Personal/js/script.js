ee5e18469eb0432e2c9e9047a5ee1a8089c58455
Corey M <CoreyRyan@fullsail.edu>
2014-02-27 23:51:50 -0500

    Functions_Personal

--- wpf/Functions_Personal/js/script.js
+++ wpf/Functions_Personal/js/script.js
@@ -4,14 +4,19 @@
  Feb. 27, 2014
  calculates chances to get shiny pokemon
  */
+var standard = standardC();
+
+function standardC(){
+    console.log("You have the standard chance of 1 in 8,192 of obtaining a shiny pokemon.");
+}
 
 var wild = prompt("Will the pokemon be a wild caught pokemon? (yes or no)");
 
-(wild != "yes" || wild != "y") ? console.log("We will move on then.") : console.log("You have a 1 in 8,192 chance in catching a shiny pokemon!");
+(wild != "yes" || wild != "y") ? console.log("We will move on then.") : console.log(standard);
 
 var parent = prompt("Is one of the parents foreign or shiny? (yes or no or both)");
 
 if(parent == "yes"){
 
-   
+   console.log("Your chances have increased");
 }
