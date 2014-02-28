88963cb4d9d79d27f668d96b9fae6935603999b8
Corey M <CoreyRyan@fullsail.edu>
2014-02-27 23:24:47 -0500

    Functions_Industry

--- wpf/Functions_Industry/js/script.js
+++ wpf/Functions_Industry/js/script.js
@@ -17,14 +17,17 @@ var den2 = prompt("What is the denominator of the second fraction?");
 (den1 == 0) ? console.log("Numbers should be greater than 0.") : console.log("");
 (num2 == 0) ? console.log("Numbers should be greater than 0.") : console.log("");
 (den2 == 0) ? console.log("Numbers should be greater than 0.") : console.log("");
-// function to figure
+// function to figure the product
 function multiply(num1, num2, den1, den2){
-
+//var to find the numerator of the answer
   var prodNum = num1 * den2;
+    // var  to find the denominator of the answer
   var prodDen = den1 * num2;
+    // calculating decimal
   var product = prodNum / prodDen;
+    // returning decimal and giving appropriate decimal places
    return(product.toFixed(5));
 
 }
-
+// displaying answer
 console.log("By multiplying the fractions the answer in decimal format is "+multiply(num1, num2, den1, den2)+".");
