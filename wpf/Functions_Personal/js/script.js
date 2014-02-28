2653f7a8c922aa79021746701657004131983fca
Corey M <CoreyRyan@fullsail.edu>
2014-02-27 22:37:35 -0500

    Functions_Personal

---
+++ wpf/Functions_Personal/js/script.js
@@ -0,0 +1,13 @@
+// Corey McLauchlin
+// Feb. 27, 2014
+// Functions_Personal
+// multiplies two fractions and outputs the answer in decimal format
+
+var num1 = prompt("What is the numerator of the first fraction?");
+var den1 = prompt("What is the denominator of the first fraction?");
+var num2 = prompt("What is the numerator of the second fraction?");
+var den2 = prompt("What is the denominator of the second fraction?");
+(num1 == 0) ? console.log("The number should be greater than 0.") : console.log("Ok.");
+(den1 == 0) ? console.log("The number should be greater than 0.") : console.log("Ok.");
+(num2 == 0) ? console.log("The number should be greater than 0.") : console.log("Ok.");
+(den2 == 0) ? console.log("The number should be greater than 0.") : console.log("Ok.");
