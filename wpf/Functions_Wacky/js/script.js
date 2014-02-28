317b15a7fcad33c53fa744b2c26766a30b5aa06e
Corey M <CoreyRyan@fullsail.edu>
2014-02-27 21:42:00 -0500

    Functions_Wacky

--- wpf/Functions_Wacky/js/script.js
+++ wpf/Functions_Wacky/js/script.js
@@ -1,3 +1,17 @@
-/**
- * Created by bangalorn on 2/27/14.
- */
+/*
+ Corey McLauchlin
+ Feb. 27, 2014
+ Functions Wacky
+ calculate mach speed
+*/
+
+var dist = prompt("How far did the object travel?");
+var time = prompt("How long did it take to travel the distance?");
+if(dist == "" || dist == 0){
+    console.log("Please enter a proper value.");
+}else if(time == "" || time == 0){
+    console.log("Please enter a proper value.");
+}
+var soundSPD = 343;
+
+
