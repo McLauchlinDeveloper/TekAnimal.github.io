fdbfe1902e2cb791174195f38d0996a16b5d9094
Corey M <CoreyRyan@fullsail.edu>
2014-02-27 23:22:17 -0500

    Functions_Industry

--- wpf/Functions_Industry/js/script.js
+++ wpf/Functions_Industry/js/script.js
@@ -3,15 +3,21 @@
 // Functions_Personal
 // multiplies two fractions and outputs the answer in decimal format
 
-var num1 = prompt("What is the numerator of the first fraction?");
-var den1 = prompt("What is the denominator of the first fraction?");
-var num2 = prompt("What is the numerator of the second fraction?");
-var den2 = prompt("What is the denominator of the second fraction?");
-(num1 == 0) ? console.log("The number should be greater than 0.") : console.log("Ok.");
-(den1 == 0) ? console.log("The number should be greater than 0.") : console.log("Ok.");
-(num2 == 0) ? console.log("The number should be greater than 0.") : console.log("Ok.");
-(den2 == 0) ? console.log("The number should be greater than 0.") : console.log("Ok.");
 
+// variable for numerator of first fraction
+var num1 = prompt("What is the numerator of the first fraction?");
+// variable for denominator of first fraction
+var den1 = prompt("What is the denominator of the first fraction?");
+// variable for numerator of second fraction
+var num2 = prompt("What is the numerator of the second fraction?");
+// variable for denominator of second fraction
+var den2 = prompt("What is the denominator of the second fraction?");
+// ternaries making sure all numbers are greater zero
+(num1 == 0) ? console.log("Numbers should be greater than 0.") : console.log("");
+(den1 == 0) ? console.log("Numbers should be greater than 0.") : console.log("");
+(num2 == 0) ? console.log("Numbers should be greater than 0.") : console.log("");
+(den2 == 0) ? console.log("Numbers should be greater than 0.") : console.log("");
+// function to figure
 function multiply(num1, num2, den1, den2){
 
   var prodNum = num1 * den2;
