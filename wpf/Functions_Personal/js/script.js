06b5676e6d52b832575e359a7136e31fbf68626b
Corey M <CoreyRyan@fullsail.edu>
2014-02-27 23:46:29 -0500

    Functions_Personal

--- wpf/Functions_Personal/js/script.js
+++ wpf/Functions_Personal/js/script.js
@@ -5,4 +5,13 @@
  calculates chances to get shiny pokemon
  */
 
-var 
+var wild = prompt("Will the pokemon be a wild caught pokemon? (yes or no)");
+
+(wild != "yes" || wild != "y") ? console.log("We will move on then.") : console.log("You have a 1 in 8,192 chance in catching a shiny pokemon!");
+
+var parent = prompt("Is one of the parents foreign or shiny? (yes or no or both)");
+
+if(parent == "yes"){
+
+   
+}
