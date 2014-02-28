ee2333335bc8f801eb2059e475731b04439c2f67
Corey M <CoreyRyan@fullsail.edu>
2014-02-27 22:08:11 -0500

    Functions_Wacky

--- wpf/Functions_Wacky/js/script.js
+++ wpf/Functions_Wacky/js/script.js
@@ -5,13 +5,26 @@
  calculate mach speed
 */
 
-var dist = prompt("How far did the object travel?");
-var time = prompt("How long did it take to travel the distance?");
+var dist = prompt("How far did the object travel? (in meters)");
+var time = prompt("How long did it take to travel the distance? (in minutes)");
 if(dist == "" || dist == 0){
     console.log("Please enter a proper value.");
 }else if(time == "" || time == 0){
     console.log("Please enter a proper value.");
 }
 var soundSPD = 343;
+
+var velocity = function velocity(){
+    vel = dist / time;
+    return(vel);
+}
+
+
+function machSPD(){
+var machSpeed = velocity() / soundSPD;
+    return(machSpeed.toFixed(0));
+}
+
+console.log("The mach speed of your object is: "); console.log(machSPD());
 
 
